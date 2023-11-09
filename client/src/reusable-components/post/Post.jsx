168efcb8165c20a3613a233c9a0301c344dc6aa5
snplmntn <124994202+snplmntn@users.noreply.github.com>
2023-11-09 21:24:26 +0800

    Connected to Backend
    Connected Login
    Connected Posts
    Connected Announcements
    Decoded User from Token

--- client/src/reusable-components/post/Post.jsx
+++ client/src/reusable-components/post/Post.jsx
@@ -1,6 +1,6 @@
 import "./Post.css";
 
-export default function Post() {
+export default function Post({ fullname, username, content, date }) {
   return (
     <div className="post">
       <div className="post-content-container">
@@ -8,23 +8,15 @@ export default function Post() {
           <div className="post-author-info">
             <div className="profile-pic"></div>
             <div className="post-author">
-              <p className="display-name">Juan Dela Cruz</p>
-              <p className="username">@user_name</p>
-              <p className="date">10/28/2023 6:09 PM</p>
+              <p className="display-name">{fullname}</p>
+              <p className="username">@{username}</p>
+              <p className="date">{date}</p>
             </div>
           </div>
           <div className="report-post-container"></div>
         </div>
         <div className="post-content">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ex
-            cumque ut suscipit officiis aut qui vitae doloremque. Tempore error
-            illo, atque velit nostrum mollitia. Distinctio quae inventore quod
-            sunt! Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
-            ex cumque ut suscipit officiis aut qui vitae doloremque. Tempore
-            error illo, atque velit nostrum mollitia. Distinctio quae inventore
-            quod sunt!
-          </p>
+          <p>{content}</p>
         </div>
       </div>
       <div className="post-interaction">{/* like and comment container */}</div>
